dfddac73298a0898068e962ee0049ba36424c0e9
Ale <ale.surdu29@gmail.com>
2023-02-02 13:45:17 +0100

    Make game round more readable

--- script.js
+++ script.js
@@ -3,36 +3,41 @@ function getComputerChoice() {
     return possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection){
     let playerChoice = playerSelection.toLowerCase();
-    if(playerChoice === 'rock') {
-        if(computerSelection === 'Paper') {
-            return 'You lose! Paper beats Rock.';
-        } else if (computerSelection === 'Scissors') {
-            return 'You win! Rock beats Scissors.';
-        } else return "It's a tie!";
-        
-    } else if(playerChoice === 'paper') {
-        if(computerSelection === 'Scissors') {
-            return 'You lose! Scissors beats Paper.';
-        } else if (computerSelection === 'Rock') {
-            return 'You win! Rock beats Paper.';
-        } else return "It's a tie!";
-    } else if(playerChoice === 'scissors') {
-        if(computerSelection === 'Rock') {
-            return 'You lose! Rock beats Scissors.';
-        } else if (computerSelection === 'Paper') {
-            return 'You win! Scissors beats Paper.';
-        } else return "It's a tie!";
-    } else return "Please choose Rock, Paper or Scissors."
-
+    switch(playerChoice){
+        case 'rock':
+            switch(computerSelection){
+                case 'Paper': 
+                    return 'You lose! Paper beats Rock.';
+                case 'Scissors':
+                    return 'You win! Rock beats Scissors.';
+                case 'Rock': 
+                    return "It's a tie!";
+            }
+        case 'paper':
+            switch(computerSelection){
+                case 'Scissors': 
+                    return 'You lose! Scissors beats Paper.';
+                case 'Rock':
+                    return 'You win! Paper beats Rock.';
+                case 'Paper': 
+                    return "It's a tie!";
+            }
+        case 'scissors':
+            switch(computerSelection){
+                case 'Rock': 
+                    return 'You lose! Rock beats scissors.';
+                case 'Paper':
+                    return 'You win! Scissors beats Paper.';
+                case 'Scissors': 
+                    return "It's a tie!";
+            }
+        default: return 'Please choose Rock, Paper or Scissors.';
+    }
 }
 
-
-
-// console.log(getComputerChoice());
-
-const playerSelection = 'rOCk';
+const playerSelection = 'pApEr';
 const computerSelection = getComputerChoice();
 
 console.log(playRound(playerSelection, computerSelection));
