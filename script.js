d3664561ca15afefaa4f3f42f04d83234d0c12e8
Ale <ale.surdu29@gmail.com>
2023-02-02 13:18:29 +0100

    Add one game round

--- script.js
+++ script.js
@@ -3,4 +3,36 @@ function getComputerChoice() {
     return possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.toLowerCase();
+    if(playerChoice === 'rock') {
+        if(computerSelection === 'Paper') {
+            return 'You lose! Paper beats Rock.';
+        } else if (computerSelection === 'Scissors') {
+            return 'You win! Rock beats Scissors.';
+        } else return "It's a tie!";
+        
+    } else if(playerChoice === 'paper') {
+        if(computerSelection === 'Scissors') {
+            return 'You lose! Scissors beats Paper.';
+        } else if (computerSelection === 'Rock') {
+            return 'You win! Rock beats Paper.';
+        } else return "It's a tie!";
+    } else if(playerChoice === 'scissors') {
+        if(computerSelection === 'Rock') {
+            return 'You lose! Rock beats Scissors.';
+        } else if (computerSelection === 'Paper') {
+            return 'You win! Scissors beats Paper.';
+        } else return "It's a tie!";
+    } else return "Please choose Rock, Paper or Scissors."
+
+}
+
+
+
+// console.log(getComputerChoice());
+
+const playerSelection = 'rOCk';
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
