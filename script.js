04f5fbf9e0275cd9440d57cdfff550f02a6498f4
Ale <ale.surdu29@gmail.com>
2023-02-02 14:16:11 +0100

    Add multiple rounds and scores

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let winner = '';
+
 function getComputerChoice() {
     let possibleChoices = ['Rock', 'Paper', 'Scissors'];
     return possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
@@ -5,30 +7,31 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection){
     let playerChoice = playerSelection.toLowerCase();
+
     switch(playerChoice){
         case 'rock':
             switch(computerSelection){
-                case 'Paper': 
+                case 'Paper': winner = 'computer';
                     return 'You lose! Paper beats Rock.';
-                case 'Scissors':
+                case 'Scissors': winner = 'player';
                     return 'You win! Rock beats Scissors.';
                 case 'Rock': 
                     return "It's a tie!";
             }
         case 'paper':
             switch(computerSelection){
-                case 'Scissors': 
+                case 'Scissors': winner = 'computer';
                     return 'You lose! Scissors beats Paper.';
-                case 'Rock':
+                case 'Rock': winner = 'player';
                     return 'You win! Paper beats Rock.';
                 case 'Paper': 
                     return "It's a tie!";
             }
         case 'scissors':
             switch(computerSelection){
-                case 'Rock': 
+                case 'Rock': winner = 'computer';
                     return 'You lose! Rock beats scissors.';
-                case 'Paper':
+                case 'Paper': winner = 'player';
                     return 'You win! Scissors beats Paper.';
                 case 'Scissors': 
                     return "It's a tie!";
@@ -37,7 +40,33 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for(let i = 0; i < 5; i++){
+        computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        if(winner === 'player') {
+            playerScore += 1;
+        } else if (winner === 'computer') {
+            computerScore += 1;
+        }
+        console.log(`Player score: ${playerScore}`);
+        console.log(`Computer score: ${computerScore}`);
+        // winner = '';
+    }
+
+    if(playerScore > computerScore) {
+        console.log('You win the game!');
+    } else if (computerScore > playerScore) {
+        console.log('Computer wins the game!');
+    } else console.log("It's a tie.");
+
+}
+
 const playerSelection = 'pApEr';
-const computerSelection = getComputerChoice();
+let computerSelection;
 
 console.log(playRound(playerSelection, computerSelection));
+game();
